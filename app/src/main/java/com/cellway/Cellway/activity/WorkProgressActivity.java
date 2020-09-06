bf3ec340b9d94147d1712c74a6512961514b102c
savej raees <savejsaifi125@gmail.com>
2020-09-06 18:09:54 +0530

    Admob add

--- app/src/main/java/com/cellway/Cellway/activity/WorkProgressActivity.java
+++ app/src/main/java/com/cellway/Cellway/activity/WorkProgressActivity.java
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.cellway.Cellway.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class WorkProgressActivity extends AppCompatActivity {
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_progress);
+
+
+
+        MobileAds.initialize(this, "ca-app-pub-2957063050547520~9759537079");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
